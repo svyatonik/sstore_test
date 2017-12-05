07e5ce2fdb263ad32bdb6e10690a4c2aed7d67ac
Svyatoslav Nikolsky <svyatonik@gmail.com>
2017-12-05 18:39:48 +0300

    auto_migrate tests

---
+++ auto_migrate/start.sh
@@ -0,0 +1,9 @@
+NUM_NODES=`ls -1 | grep .toml | wc -l`
+echo $NUM_NODES
+
+#NUM_NODES=$(($NUM_NODES-1))
+
+for i in `seq 1 $NUM_NODES`
+do
+	RUST_LOG=secretstore=trace ../parity --config ./poa_ss${i}.toml&
+done
